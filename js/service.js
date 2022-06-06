2980de0dc44bf6f4f4a520704375cee4db772ecf
Antonio Henriques de França Terceiro <antoniomed1.an@gmail.com>
2022-06-05 22:47:36 -0300

    adiciona margem aos botoes

--- js/service.js
+++ js/service.js
@@ -12,45 +12,6 @@ function randomIntFromInterval(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let mockData = [
-	{
-		id: 96,
-		nome: "Centrexin",
-		valor: 0.24,
-		data: "1993-12-03T03:00:54.115Z",
-	},
-	{
-		id: 78,
-		nome: "Orbalix",
-		valor: 5.89,
-		data: "1984-06-05T04:41:26.067Z",
-	},
-	{
-		id: 95,
-		nome: "Emergent",
-		valor: 8.37,
-		data: "1996-05-07T12:47:02.497Z",
-	},
-	{
-		id: 21,
-		nome: "Balooba",
-		valor: 6.75,
-		data: "2018-03-25T10:31:35.529Z",
-	},
-	{
-		id: 4,
-		nome: "Chorizon",
-		valor: 1.05,
-		data: "1993-06-10T19:31:14.797Z",
-	},
-	{
-		id: 85,
-		nome: "Retrack",
-		valor: 6.72,
-		data: "1977-08-02T00:30:58.902Z",
-	},
-];
-
 function formatDate(date, isDMY = false) {
 	const dd = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
 	const mm =
@@ -111,7 +72,7 @@ function showGastos(gastos) {
 				"</td>" +
 				"<td>" +
 				`<div class="d-grid gap-2 d-md-flex justify-content-md-end">
-					<button onClick="editGasto(${gasto.id}, \'${gasto.nome}\', ${gasto.valor}, \'${gasto.data}\')" type="button" class="btn btn-warning">Editar</button> <button onClick="deleteGasto(${gasto.id})" type="button" class="btn btn-danger">Deletar</button>
+					<button onClick="editGasto(${gasto.id}, \'${gasto.nome}\', ${gasto.valor}, \'${gasto.data}\')" type="button" class="btn btn-warning me-2">Editar</button> <button onClick="deleteGasto(${gasto.id})" type="button" class="btn btn-danger">Deletar</button>
 				</div>` +
 				"</td>" +
 				"</tr>"
